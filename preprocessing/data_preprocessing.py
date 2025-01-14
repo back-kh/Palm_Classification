7a8c45b0dc8f5d76a283d6632647b66cf56ad898
Nimol <thuonnimol1@gmail.com>
2025-01-15 03:51:11 +0700

    Update data_preprocessing.py

--- preprocessing/data_preprocessing.py
+++ preprocessing/data_preprocessing.py
@@ -14,12 +14,7 @@ def load_data():
     df_train = pd.read_csv(TRAIN_LABEL_PATH, sep=';', header=None, names=['img_path', 'text']).dropna().reset_index()
     df_test = pd.read_csv(TEST_LABEL_PATH, sep=';', header=None, names=['img_path', 'text']).dropna().reset_index()
     return df_train, df_test
-
-def load_classes():
-    with open("Text-Classification/list_class_name.txt", 'r') as file:
-        vocab = [i_word for i_word in file.read().split("\n") if i_word != '']
-    return {label: idx for idx, label in enumerate(vocab)}
-
+    
 def encode_labels(df_train, df_test, target_dict):
     le = preprocessing.LabelEncoder()
     le.fit(list(target_dict.keys()))
@@ -31,6 +26,12 @@ def encode_labels(df_train, df_test, target_dict):
     df_test['img_path'] = df_test.img_path.apply(lambda x: os.path.join(BASE_TEST_DIR, x))
     
     return df_train, df_test
+def load_classes():
+    with open("Text-Classification/list_class_name.txt", 'r') as file:
+        vocab = [i_word for i_word in file.read().split("\n") if i_word != '']
+    return {label: idx for idx, label in enumerate(vocab)}
+
+
 
 def create_data_generators(df_train, df_test):
     target_dict = load_classes()
